c56f9fc70a4ccda7f5d50a1b21305fd5b0376951
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2023-12-10 13:00:50 +0100

    Parse game IDs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,31 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-    val input = File("input.txt").readLines().dropLast(1)
+    val input = File("input.txt").readLines()
 
+    input.forEach { println(it.parseGame()) }
+}
 
+data class Game(
+    val id: Int,
+    val subsets: List<Subset>,
+)
+
+data class Subset(
+    val red: Int,
+    val green: Int,
+    val blue: Int,
+)
+
+fun String.parseGame(): Game {
+    val gameTokens = split(":")
+    val gameToken = gameTokens.first()
+    val id = """Game (\d+)""".toRegex()
+        .find(gameToken)!!.groupValues[1].toInt()
+
+    return Game(id, emptyList())
+}
+
+fun String.parseSubset(): Subset {
+    TODO()
 }
